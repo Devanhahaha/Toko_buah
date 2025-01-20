fc3926120bd99d9829cab4f75485c799cd841c16
Devanhahaha <devanherdiansyah74@gmail.com>
2025-01-20 11:44:27 +0700

    solved

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -98,6 +98,9 @@
             </form>
         </div>
       </section>
+      <div class="copyright">
+        <p>&#169; Toko Buah Devan All Right Reserved.</p>
+      </div>
 
       <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 
